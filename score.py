b4cbb0831a52723c3e1c0ef1179a922d09df3057
Sida Cheng <>
2022-08-08 13:38:22 -0400

    TP1

--- score.py
+++ score.py
@@ -8,9 +8,7 @@ class ScoreCounter(Player):
         self.yaku = list() #役种，因为可以同时达成多个役种，所以是list而非单个变量
         
 
-# Note: Due to time constraint, I will not complete this part for TP
-#       Plus, for a deck tracker, this doesn't matter that much
 # TODO: Organize hand into groups (may have more than one ways to organize)
-# TODO: Catogrize group for detecting yaku(chi,pon,)
+# TODO: Catogrize group for detecting yaku(chi,pon,kan,etc)
 # TODO: Detect every kind of yaku (some yaku may not happen at the same time, ex:两杯口和七对子)
 
